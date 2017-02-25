c71dc36b76fc2957ebc5f306923ed527ffad7740
brownae <aebrown9@gmail.com>
2017-02-24 19:03:42 -0800

    initial commit
    Just the html and css for home page.

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The News Stand</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <div class="banner">
+    <img class="corner" src="images/corner-triangle-news.png" alt="News">
+  </div>
+  <div class="panels">
+    <div class="panel panel1">
+      <p>Tech</p>
+      <ul>
+        <li><a href="#">Companies</a></li>
+        <li><a href="#">New Gadgets</a></li>
+        <li><a href="#">New Discoveries</a></li>
+      </ul>
+    </div>
+    <div class="panel panel2">
+      <p>Sports</p>
+      <ul>
+        <li><a href="#">Companies</a></li>
+        <li><a href="#">New Gadgets</a></li>
+        <li><a href="#">New Discoveries</a></li>
+      </ul>
+    </div>
+    <div class="panel panel3">
+      <p>Art</p>
+      <ul>
+        <li><a href="#">Companies</a></li>
+        <li><a href="#">New Gadgets</a></li>
+        <li><a href="#">New Discoveries</a></li>
+      </ul>
+    </div>
+
+  </div>
+
+<script src="js/script.js" charset="utf-8"></script>
+</body>
+</html>
