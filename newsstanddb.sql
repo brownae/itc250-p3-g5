27a1e3be777bce3f88bb34d0d05143c44b6b1374
brownae <aebrown9@gmail.com>
2017-03-02 11:21:28 -0800

    Adds prefix of NEWS_ to Category & Feed

--- newsstanddb.sql
+++ newsstanddb.sql
@@ -5,47 +5,47 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS Category;
-CREATE TABLE Category (
+DROP TABLE IF EXISTS NEWS_Category;
+CREATE TABLE NEWS_Category (
     CategoryID INT unsigned NOT NULL AUTO_INCREMENT,
     Subject VARCHAR(255),
     Description TEXT,
     DateAdded DATETIME,
-    LastUpdated TIMESTAMP 
+    LastUpdated TIMESTAMP
         DEFAULT 0 ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (CategoryID)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO Category 
-(Subject, Description, DateAdded) 
+INSERT INTO NEWS_Category
+(Subject, Description, DateAdded)
 VALUES
 ('Art', 'Keep up with the most innovative artists around the world!',  Now()),
 ('Technology', 'All the latest tech news!',  Now()),
 ('Sports', 'Get the latest scores, updates and whatever else sports dudes talk about!',  Now());
 
-DROP TABLE IF EXISTS Feed;
-CREATE TABLE Feed (
+DROP TABLE IF EXISTS NEWS_Feed;
+CREATE TABLE NEWS_Feed (
     FeedID INT unsigned NOT NULL AUTO_INCREMENT,
     CategoryID INT unsigned DEFAULT '0',
     Subject VARCHAR(255),
     Description TEXT,
     DateAdded DATETIME,
-    LastUpdated TIMESTAMP 
+    LastUpdated TIMESTAMP
         DEFAULT 0 ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (FeedID),
   INDEX ux_CategoryID (CategoryID),
-  CONSTRAINT fk_Category_Feed 
-    FOREIGN KEY (CategoryID) 
-    REFERENCES Category(CategoryID) 
+  CONSTRAINT fk_Category_Feed
+    FOREIGN KEY (CategoryID)
+    REFERENCES NEWS_Category(CategoryID)
     ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 SELECT @ID := CategoryID
-    FROM Category
+    FROM NEWS_Category
     WHERE Subject = 'Art';
 
-INSERT INTO Feed 
-(CategoryID, Subject, Description, DateAdded) 
+INSERT INTO NEWS_Feed
+(CategoryID, Subject, Description, DateAdded)
 VALUES
 #Art
 (@ID,	'Painting',	'News on painting',	Now()),
@@ -53,11 +53,11 @@ VALUES
 (@ID,	'Dance',	'Dancing news',	Now());
 
 SELECT @ID :=  CategoryID
-    FROM Category
+    FROM NEWS_Category
     WHERE Subject = 'Technology';
 
-INSERT INTO Feed 
-(CategoryID, Subject, Description, DateAdded) 
+INSERT INTO NEWS_Feed
+(CategoryID, Subject, Description, DateAdded)
 #Tech
 Values
 (@ID,	'Companies',	'What tech companies are making the news?',	Now()),
@@ -65,11 +65,11 @@ Values
 (@ID,	'New Discoveries',	'What will be discovered next?',	    Now());
 
 SELECT @ID := CategoryID
-    FROM Category
+    FROM NEWS_Category
     WHERE Subject = 'Sports';
 
-INSERT INTO Feed 
-(CategoryID, Subject, Description, DateAdded) 
+INSERT INTO NEWS_Feed
+(CategoryID, Subject, Description, DateAdded)
 #Sports
 Values
 (@ID,	'Running',	'Running news',	 Now()),
