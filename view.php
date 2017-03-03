99bbb8ac8a633807161499c770bcd71a4d8ce866
mcdavis19 <mcd6@uw.edu>
2017-03-02 18:24:52 -0800

    Adds SQL and RSS URL to view.php
    Now this page has most of the tools we need to display the RSS feed.

--- view.php
+++ view.php
@@ -1,6 +1,39 @@
-<?php include 'includes/header.php'; ?>
+<?php 
+
+include './includes/header.php';
+require './includes/config.php';
+
+if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystring
+	 $myID = (int)$_GET['id']; #Convert to integer, will equate to zero if fails
+}
+
+$sql = 'SELECT * 
+        FROM Feed
+        WHERE FeedID = :id ;
+    ';
+
+$db = db_conn();            //Make Database connection
+$sql = $db->prepare($sql);  //Prepare SQL statement
+$sql->bindParam(':id', $myID, PDO::PARAM_INT);
+$sql->execute();            //Execute SQL
+$results = $sql->fetchAll();//Store results
+$sql->closeCursor();        //Close the connection for safety
+
+?>
 
 <h3>Test view.php</h3>
 
+<!--RSS URL-->
+<!--https://news.google.com/news?cf=all&hl=en&pz=1&ned=' . SUBJECT . '&output=rss-->
+
+<pre>
+<?php 
+var_dump($_GET); 
+var_dump($results); 
+$subject = str_replace(' ', '+', $results['Subject']);
+$url = 'https:news.google.com/news?cf=all&hl=en&pz=1&ned=' . $subject  . '&output=rss';
+echo $url;
+?>
+</pre>
 
 <?php include 'includes/footer.php'; ?>
