ba60c13d450445cead21c5009ef6cdb95e31ed83
ugraphix <grace@ugraphix.com>
2017-03-06 21:47:50 -0800

    Session Cache
    Session cache complete. Saves RSS feed content to database. If feed
    hasnt been updated in 15min or longer, it pulls new content, updates,
    and saves to the database.

--- view.php
+++ view.php
@@ -27,12 +27,44 @@ $sql->closeCursor();        //Close the connection for safety
 <!--https://news.google.com/news?cf=all&hl=en&pz=1&ned=' . SUBJECT . '&output=rss-->
 
 <pre>
-<?php 
-var_dump($_GET); 
-var_dump($results); 
-$subject = str_replace(' ', '+', $results['Subject']);
-$url = 'https:news.google.com/news?cf=all&hl=en&pz=1&ned=' . $subject  . '&output=rss';
-echo $url;
+<?php
+
+//var_dump($_GET); 
+//var_dump($results); 
+
+$feed = $results[0];
+
+$now = time();
+$last_update_time = strtotime($feed['LastUpdated']);
+$age = $now - $last_update_time;
+echo "Last download was " . $age . " seconds ago. <br>";
+
+if ($age > 30) {
+    // If the saved feed is more than 15 minutes old, download it from Google
+    // and write it to the database.
+    $subject = str_replace(' ', '+', $feed['Subject']);
+    $url = 'https://news.google.com/news?cf=all&hl=en&pz=1&ned=' . $subject  . '&output=rss';
+    echo "Cache is too old, downloading from " . $url . '<br>';
+    $rss_feed = file_get_contents($url) . "<br>";
+
+    $sql = 'UPDATE Feed
+            SET LastUpdated=NOW(),SavedFeed=:feed
+            WHERE FeedID = :id';
+    $sql = $db->prepare($sql);
+    $sql->bindParam(':id', $myID, PDO::PARAM_INT);
+    $sql->bindParam(':feed', $rss_feed, PDO::PARAM_STR);
+    $sql->execute();
+    $sql->closeCursor();
+} else {
+    // If saved feed is less than 15 minutes old, use the data we have saved
+    // in the database.
+    $rss_feed = $feed['SavedFeed'];
+}
+
+echo $rss_feed;
+
+
+
 ?>
 </pre>
 
