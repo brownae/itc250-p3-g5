25c372e4dbd6542d532162eaa93996e08c93facb
brownae <aebrown9@gmail.com>
2017-03-09 17:39:39 -0800

    Updates the way $TimeCreated and $Expire are populated

--- NewsStand/RssNews.php
+++ NewsStand/RssNews.php
@@ -2,9 +2,12 @@
 
 class RssNews
 {
-	 public $NewsID = 0;
-	 public $FeedXML = "";
-	 public $TimeCreated = 0;
+    const CACHE_TIME = 3 * 60; //3 minutes
+    public $NewsID = 0;
+    public $FeedXML = "";
+    public $TimeCreated = 0;
+    public $Expire = 0;
+
 
 	/**
 	 * Constructor for Answer class.
@@ -15,10 +18,11 @@ class RssNews
 	 * @return void
 	 * @todo none
 	 */
-    function __construct($NewsId,$FeedXml,$Time)
+    function __construct($NewsId,$FeedXml)
 	{#constructor sets stage by adding data to an instance of the object
 		$this->NewsID = (int)$NewsId;
 		$this->FeedXML = $FeedXml;
-		$this->TimeCreated = $Time;
+		$this->TimeCreated = time();
+    $this->Expire = time() + CACHE_TIME ;
 	}#end RssNews() constructor
 }#end RssNews class
